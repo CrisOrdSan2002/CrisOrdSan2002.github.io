ee15c900b40feb510c7c11062558fce318b4c209
Cristian Gilberto Ordoñez Santiago <cris.ordonez.santiago@gmail.com>
2026-07-21 15:24:14 -0600

    Update event location links in script.js

--- xv-years-julia-aide/script.js
+++ xv-years-julia-aide/script.js
@@ -1,9 +1,11 @@
 // Configuración rápida
 const EVENT_DATE = "2026-08-22T00:00:00-06:00";
 
-// Cambia estos enlaces cuando tengas la información real.
-const GOOGLE_MAPS_URL = "https://maps.google.com/?q=Ubicaci%C3%B3n%20pendiente";
-const GOOGLE_MAPS_EMBED = "https://www.google.com/maps?q=Ubicaci%C3%B3n%20pendiente&output=embed";
+// Ubicaciones del evento
+const CHURCH_MAPS_URL = "https://maps.app.goo.gl/QSF9zLJrNTh5Vtn19";
+const CHURCH_MAPS_EMBED = "https://www.google.com/maps/embed?pb=!3m2!1ses!2smx!4v1784668301870!5m2!1ses!2smx!6m8!1m7!1sKN9k195KXv3crXOpvYBW-g!2m2!1d20.55066739965147!2d-97.44529894757447!3f302.15052607182895!4f1.2750666081755497!5f0.4000000000000002https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1276.9239974956818!2d-97.44634638548956!3d20.550667399651477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85da6aace89c1daf%3A0xfb62f7482dfdffca!2sParroquia%20de%20Nuestra%20Se%C3%B1ora%20de%20Guadalupe!5e1!3m2!1ses!2smx!4v1784668687239!5m2!1ses!2smx";
+const VENUE_MAPS_URL = "https://maps.app.goo.gl/SoKTaF3D4cuVYh368";
+const VENUE_MAPS_EMBED = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10216.826389541577!2d-97.43607868804817!3d20.529196480918756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85da41d32e2fd45b%3A0x646179e226f67a39!2sSamsara%20Sal%C3%B3n%20de%20Eventos!5e1!3m2!1ses!2smx!4v1784668723497!5m2!1ses!2smx";
 const WHATSAPP_CONFIRM_URL = "https://wa.me/5217841261547?text=Hola%2C%20confirmo%20mi%20asistencia%20a%20los%20XV%20a%C3%B1os%20de%20Julia%20Aid%C3%A9.%20";
 
 const $ = (selector) => document.querySelector(selector);
@@ -44,8 +46,10 @@ updateCountdown();
 setInterval(updateCountdown, 1000);
 
 // Maps y confirmación
-$("#mapsBtn").href = GOOGLE_MAPS_URL;
-$("#mapFrame").src = GOOGLE_MAPS_EMBED;
+$("#churchMapsBtn").href = CHURCH_MAPS_URL;
+$("#churchMapFrame").src = CHURCH_MAPS_EMBED;
+$("#venueMapsBtn").href = VENUE_MAPS_URL;
+$("#venueMapFrame").src = VENUE_MAPS_EMBED;
 $("#confirmBtn").href = WHATSAPP_CONFIRM_URL;
 
 // Animación al hacer scroll
